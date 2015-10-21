579be68bf9ccc80dfe7cf20acbd6ad469338b628
Mary Rose Cook <mary@maryrosecook.com>
2015-10-21 15:43:43 -0400

    Add bresenham-line fn

--- src/tac/core.cljs
+++ src/tac/core.cljs
@@ -41,6 +41,32 @@
       (assoc-in (reduce #(%2 %1) state moves) [:player :last-move] (now))
       state)))
 
+;; stolen from github.com/jackschaedler/goya/blob/master/src/cljs/goya/components/bresenham.cljs
+(defn bresenham-line [{x0 :x y0 :y} {x1 :x y1 :y}]
+  (let [len-x (js/Math.abs (- x1 x0))
+        len-y (js/Math.abs (- y1 y0))
+        is-steep (> len-y len-x)]
+    (let [[x0 y0 x1 y1] (if is-steep [y0 x0 y1 x1] [x0 y0 x1 y1])]
+      (let [[x0 y0 x1 y1] (if (> x0 x1) [x1 y1 x0 y0] [x0 y0 x1 y1])]
+        (let [delta-x (- x1 x0)
+              delta-y (js/Math.abs (- y1 y0))
+              y-step (if (< y0 y1) 1 -1)]
+          (loop [x x0
+                 y y0
+                 error (js/Math.floor (/ delta-x 2))
+                 pixels (if is-steep [{:x y :y x}] [{:x x :y y}])]
+            (if (> x x1)
+              pixels
+              (if (< error delta-y)
+                (recur (inc x)
+                       (+ y y-step)
+                       (+ error (- delta-x delta-y))
+                       (if is-steep (conj pixels {:x y :y x}) (conj pixels {:x x :y y})))
+                (recur (inc x)
+                       y
+                       (- error delta-y)
+                       (if is-steep (conj pixels {:x y :y x}) (conj pixels {:x x :y y}))
+                       )))))))))
 
 (defn step [state]
   (move-player state))
